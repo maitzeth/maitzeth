82123c0f2ec44c51ce885d26dbe19d82fd41e452
andre801mz@gmail.com <andre801mz@gmail.com>
2021-03-27 11:18:55 -0300

    Fixing merge conflict con _app

--- helpers/useApiErrorHandler.js
+++ helpers/useApiErrorHandler.js
@@ -3,13 +3,10 @@ const useApiErrorHandler = (fn) => async (req, res) => {
     const response = await fn(req, res);
     return response;
   } catch (error) {
-    console.log(error);
-
     if (error.response) {
       return res.status(error.response.status).json({
         status: error.response.status,
         message: error.message,
-        token: process.env.GITHUB_SECRET,
       });
     }
 
