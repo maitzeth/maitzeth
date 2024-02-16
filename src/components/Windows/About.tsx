3fb8c99203e36124fc40011272ad9958f0e3eb56
maitzeth <andre801mz@gmail.com>
2024-02-16 19:11:09 -0300

    chore: updating intro

--- src/components/Windows/About.tsx
+++ src/components/Windows/About.tsx
@@ -1,7 +1,7 @@
 import { differenceInYears } from 'date-fns';
 import { WindowLayout } from './Layout';
 import { Stack, Title, Paragraph, Container } from '../UI';
-import PhotoOfMe from '../../assets/me.png';
+import PhotoOfMe from '@/assets/me.png';
 
 const About = () => {
   const workingTime = differenceInYears(new Date(), new Date(2015, 1, 1));
@@ -13,16 +13,16 @@ const About = () => {
           <Stack direction={{ mobile: 'vertical', desktop: 'vertical' }} space={{ mobile: 2, desktop: 2 }}>
             <Title component='h1'>Hello there 👋, I'm André Iván.</Title>
             <Paragraph>
-            I'm a Software Engineer, currently based in Argentina. With over {workingTime} years of experience in web development, I have been providing expert consultancy services to numerous clients and companies.
+              I'm a Software Engineer, currently based in Argentina. With over 9 years of experience in web development, I have been providing expert consultancy services to numerous clients and companies.
             </Paragraph>
             <Paragraph>
-              As a Front End developer, I love crafting seamless user experiences and interfaces with beautiful and developer-friendly code. Over the past couple of years, my main focus has been around Design Systems, Performance, Accessibility in Web Development using React and its ecosystem.
+              I'm a Software Engineer that's passionate about building UI's and love crafting seamless user experiences and interfaces with beautiful and developer-friendly code. Over the past couple of years, my main focus has been around Design Systems, Performance, Accessibility in Web Development using React and its ecosystem.
             </Paragraph>
             <Paragraph>
-              This doesn't mean I don't like to work with Back End technologies. However, I'm a pragmatic person :)
+              My strongest skills are around frontend development. However, I'm a pragmatic person :) so i can also tackle backend work.
             </Paragraph>
             <Paragraph>
-              I'd like to create software solutions that leave an impact on both users, businesses and developers.
+              I like to create software solutions that leave an impact on both users, businesses and developers.
             </Paragraph>
             <Paragraph>
               I love my cats 😼, play videogames 🎮 and drums 🥁.
