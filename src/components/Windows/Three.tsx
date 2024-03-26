4d446e7ef22cb157321ff99da43cb6d220a867b8
maitzeth <andre801mz@gmail.com>
2024-03-26 03:09:17 -0300

    adding toggler rotation on moon threejs

--- src/components/Windows/Three.tsx
+++ src/components/Windows/Three.tsx
@@ -1,4 +1,4 @@
-import { Suspense } from 'react';
+import { Suspense, useState } from 'react';
 import { WindowLayout } from './Layout';
 import { Canvas, useLoader } from '@react-three/fiber'
 import { TextureLoader } from 'three/src/loaders/TextureLoader'
@@ -6,14 +6,14 @@ import { OrbitControls } from '@react-three/drei'
 import moonTexture from '@/assets/moon.jpg';
 import starsBG from '@/assets/starsbg.jpg';
 
-function RotatingMoon() {
+function RotatingMoon({ isRotating }: { isRotating: boolean }) {
   const [colorMap] = useLoader(TextureLoader, [moonTexture]);
 
   return (
     <>
-      <OrbitControls autoRotate />
-      <ambientLight intensity={0.1}  />
-      {/* <directionalLight /> */}
+      <OrbitControls autoRotate={isRotating} />
+      <ambientLight intensity={0.08}  />
+      <directionalLight color="white" position={[1, 1, 1]} />
       <mesh scale={2.5}>
         <sphereGeometry args={[1, 200, 200]} />
         <meshStandardMaterial
@@ -25,9 +25,20 @@ function RotatingMoon() {
   );
 }
 
-export const Three = () => {      
+export const Three = () => {    
+  const [isRotating, setIsRotating] = useState(true);
+  
   return (
-    <WindowLayout title="Three JS Playground" className="p-0">
+    <WindowLayout title="Three JS Playground" className="p-0 relative">
+      <button
+        type="button"
+        className="bg-white rounded-lg absolute top-5 left-1 z-10 px-10 py-2"
+        onClick={() => {
+          setIsRotating(!isRotating);
+        }}
+      >
+        {isRotating ? 'Stop' : 'Start'} Rotation
+      </button>
       <Canvas
         style={{
           backgroundImage: `url(${starsBG})`,
@@ -37,7 +48,7 @@ export const Three = () => {
         }}
       >
         <Suspense fallback={null}>
-          <RotatingMoon />
+          <RotatingMoon isRotating={isRotating} />
         </Suspense>
       </Canvas>
     </WindowLayout>
